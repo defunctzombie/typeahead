7c327647e9a4a5282b5b6334101bf9ca1160785c
Roman Shtylman <shtylman@gmail.com>
2013-01-20 15:40:37 -0500

    js source formatting

--- typeahead.js
+++ typeahead.js
@@ -3,122 +3,131 @@ var xtend = require('xtend');
 var dom = require('dom');
 
 var defaults = {
-  source: [],
-  items: 8,
-  menu: '<ul class="typeahead dropdown-menu hidden"></ul>',
-  item: '<li><a href="#"></a></li>',
-  minLength: 1
+    source: [],
+    items: 8,
+    menu: '<ul class="typeahead hidden"></ul>',
+    item: '<li><a href="#"></a></li>',
+    minLength: 1
 }
 
 var Typeahead = function (element, options) {
-  if (!(this instanceof Typeahead)) {
-    return new Typeahead(element, options);
-  }
+    if (!(this instanceof Typeahead)) {
+        return new Typeahead(element, options);
+    }
 
-  this.element = dom(element);
-  this.options = xtend({}, defaults, options);
-  this.matcher = this.options.matcher || this.matcher
-  this.sorter = this.options.sorter || this.sorter
-  this.highlighter = this.options.highlighter || this.highlighter
-  this.updater = this.options.updater || this.updater
-  this.menu = dom(this.options.menu);
-  dom(document.body).append(this.menu);
+    var self = this;
 
-  this.source = this.options.source
-  this.shown = false
-  this.listen()
+    self.element = dom(element);
+    self.options = xtend({}, defaults, options);
+    self.matcher = self.options.matcher || self.matcher
+    self.sorter = self.options.sorter || self.sorter
+    self.highlighter = self.options.highlighter || self.highlighter
+    self.updater = self.options.updater || self.updater
+    self.menu = dom(self.options.menu);
+    dom(document.body).append(self.menu);
+
+    self.source = self.options.source;
+    self.shown = false;
+    self.listen();
 }
 
-Typeahead.prototype = {
+// for minification
+var proto = Typeahead.prototype;
 
-  constructor: Typeahead
+proto.constructor = Typeahead;
 
-, select: function () {
-    var val = this.menu.find('.active').attr('data-value');
+// select the current item
+proto.select = function() {
+    var self = this;
+    var val = self.menu.find('.active').attr('data-value');
 
-    this.element
-      .value(this.updater(val))
+    self.element
+      .value(self.updater(val))
       .emit('change');
 
-    return this.hide()
-  }
+    return self.hide();
+}
 
-, updater: function (item) {
-    return item
-  }
+proto.updater = function (item) {
+    return item;
+}
 
-, show: function () {
-    var offset = this.element.offset();
+// show the popup menu
+proto.show = function () {
+    var self = this;
+
+    var offset = self.element.offset();
     var pos = xtend({}, offset, {
-      height: this.element.outerHeight()
+        height: self.element.outerHeight()
     })
 
-    var top = pos.top + pos.height
-    var bottom = 'auto'
+    var top = pos.top + pos.height + 'px';
+    var bottom = 'auto';
 
-    if (this.options.position === 'above') {
-      top = 'auto'
-      bottom = document.body.clientHeight - pos.top + 3;
+    if (self.options.position === 'above') {
+        top = 'auto'
+        bottom = document.body.clientHeight - pos.top + 3;
     }
 
-    this.menu.css({
-      top: top,
-      bottom: bottom,
-      left: pos.left
+    self.menu.css({
+        top: top,
+        bottom: bottom,
+        left: pos.left + 'px'
     });
 
-    this.menu.removeClass('hidden');
-    this.shown = true
-    return this
-  }
+    self.menu.removeClass('hidden');
+    self.shown = true;
+    return self;
+}
 
-, hide: function () {
+// hide the popup menu
+proto.hide = function () {
     this.menu.addClass('hidden');
-    this.shown = false
-    return this
-  }
+    this.shown = false;
+    return this;
+}
 
-, lookup: function (event) {
-    var items
+proto.lookup = function (event) {
+    var self = this;
 
-    this.query = this.element.value();
+    self.query = self.element.value();
 
-    if (!this.query || this.query.length < this.options.minLength) {
-      return this.shown ? this.hide() : this
+    if (!self.query || self.query.length < self.options.minLength) {
+        return self.shown ? self.hide() : self
     }
 
-    if (this.source instanceof Function) {
-      this.source(this.query, this.process.bind(this));
+    if (self.source instanceof Function) {
+        self.source(self.query, self.process.bind(self));
     }
     else {
-      this.process(this.source);
+        self.process(self.source);
     }
 
-    return this;
-  }
+    return self;
+}
 
-, process: function (items) {
-    var self = this
+proto.process = function (items) {
+    var self = this;
 
     items = items.filter(self.matcher.bind(self));
-    items = this.sorter(items)
+    items = self.sorter(items)
 
     if (!items.length) {
-      return this.shown ? this.hide() : this
+      return self.shown ? self.hide() : self
     }
 
-    return this.render(items.slice(0, this.options.items)).show()
-  }
+    return self.render(items.slice(0, self.options.items)).show()
+}
 
-, matcher: function (item) {
-    return ~item.toLowerCase().indexOf(this.query.toLowerCase())
-  }
+proto.matcher = function (item) {
+  return ~item.toLowerCase().indexOf(this.query.toLowerCase())
+}
 
-, sorter: function (items) {
-    var beginswith = []
-      , caseSensitive = []
-      , caseInsensitive = []
-      , item
+proto.sorter = function (items) {
+    var beginswith = [];
+    var caseSensitive = [];
+    var caseInsensitive = [];
+    var item;
 
     while (item = items.shift()) {
       if (!item.toLowerCase().indexOf(this.query.toLowerCase())) beginswith.push(item)
@@ -127,57 +136,58 @@ Typeahead.prototype = {
     }
 
     return beginswith.concat(caseSensitive, caseInsensitive)
-  }
+}
 
-, highlighter: function (item) {
-    var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&')
+proto.highlighter = function (item) {
+    var query = this.query.replace(/[\-\[\]{}()*+?.,\\\^$|#\s]/g, '\\$&');
     return item.replace(new RegExp('(' + query + ')', 'ig'), function ($1, match) {
-      return '<strong>' + match + '</strong>'
+        return '<strong>' + match + '</strong>'
     })
-  }
+}
 
-, render: function (items) {
-    var self = this
+proto.render = function (items) {
+    var self = this;
 
     items = items.map(function (item) {
-      var li = dom(self.options.item);
-      li.attr('data-value', item);
-      li.find('a').html(self.highlighter(item));
-      return li
+        var li = dom(self.options.item);
+        li.attr('data-value', item)
+          .find('a').html(self.highlighter(item));
+        return li;
     })
 
     items[0].addClass('active');
 
     self.menu.empty();
     items.forEach(function(item) {
-      self.menu.append(item);
+        self.menu.append(item);
     });
-    return this
-  }
 
-, next: function (event) {
+    return this;
+}
+
+proto.next = function (event) {
     var active = this.menu.find('.active').removeClass('active');
     var next = active.next();
 
     if (!next.length) {
-      next = this.menu.find('li').first();
+        next = this.menu.find('li').first();
     }
 
-    next.addClass('active')
-  }
+    next.addClass('active');
+}
 
-, prev: function (event) {
+proto.prev = function (event) {
     var active = this.menu.find('.active').removeClass('active');
     var prev = active.prev();
 
     if (!prev.length) {
-      prev = this.menu.find('li').last();
+        prev = this.menu.find('li').last();
     }
 
-    prev.addClass('active')
-  }
+    prev.addClass('active');
+}
 
-, listen: function () {
+proto.listen = function () {
     var self = this;
 
     self.element
@@ -189,82 +199,83 @@ Typeahead.prototype = {
     self.menu
       .on('click', self.click.bind(self))
       .on('mouseenter', 'li', self.mouseenter.bind(self))
-  }
+}
 
-, move: function (e) {
+proto.move = function (e) {
     if (!this.shown) return
 
     switch(e.keyCode) {
-      case 9: // tab
-      case 13: // enter
-      case 27: // escape
+    case 9: // tab
+    case 13: // enter
+    case 27: // escape
         e.preventDefault()
         break
 
-      case 38: // up arrow
+    case 38: // up arrow
         e.preventDefault()
         this.prev()
         break
 
-      case 40: // down arrow
+    case 40: // down arrow
         e.preventDefault()
         this.next()
         break
     }
 
     e.stopPropagation()
-  }
+}
 
-, keydown: function (e) {
+proto.keydown = function (e) {
     this.suppressKeyPressRepeat = [40,38,9,13,27].indexOf(e.keyCode) >= 0
     this.move(e)
-  }
+}
 
-, keypress: function (e) {
+proto.keypress = function (e) {
     if (this.suppressKeyPressRepeat) return
     this.move(e)
-  }
+}
 
-, keyup: function (e) {
+proto.keyup = function (e) {
+    var self = this;
+
     switch(e.keyCode) {
-      case 40: // down arrow
-      case 38: // up arrow
+    case 40: // down arrow
+    case 38: // up arrow
+            break
+
+    case 9: // tab
+    case 13: // enter
+        if (!self.shown) return
+        self.select()
         break
 
-      case 9: // tab
-      case 13: // enter
-        if (!this.shown) return
-        this.select()
+    case 27: // escape
+        if (!self.shown) return
+        self.hide()
         break
 
-      case 27: // escape
-        if (!this.shown) return
-        this.hide()
-        break
-
-      default:
-        this.lookup()
+    default:
+        self.lookup()
     }
 
     e.stopPropagation()
     e.preventDefault()
 }
 
-, blur: function (e) {
-    var self = this
-    setTimeout(function () { self.hide() }, 150)
-  }
+proto.blur = function (e) {
+    var self = this;
+    setTimeout(function () { self.hide() }, 150);
+}
 
-, click: function (e) {
-    e.stopPropagation()
-    e.preventDefault()
-    this.select()
-  }
+proto.click = function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.select();
+}
 
-, mouseenter: function (e) {
+proto.mouseenter = function (e) {
     this.menu.find('.active').removeClass('active');
     dom(e.currentTarget).addClass('active');
-  }
 }
 
 module.exports = Typeahead;
